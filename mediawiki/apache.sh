b50d294a974a8c373767e730b405eb1cc17d6d6e
devopsengineer <rohitnone1998@gmail.com>
2023-06-03 15:36:12 +0530

    add roboshop nginx

--- mediawiki/apache.sh
+++ mediawiki/apache.sh
@@ -1,12 +1,10 @@
-
-
 cd /tmp
+echo "Downloading Software"
 url="https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz"
 curl -O $url
-
-
+folder=$(echo $url  | awk -F / '{print $6}' | sed 's/.tar.gz//')
 yum install httpd -y
-
-cd  /var/www/html
-
-tar - xf /tmp/mediawiki-*.tar.gz
+rm -rf /var/www/html/*
+cd /var/www/html
+tar -xf /tmp/mediawiki-1.39.3.tar.gz
+mv $folder mediawiki
